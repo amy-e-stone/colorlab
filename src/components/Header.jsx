e166a3c612a47f7da9dbaa21dfcbb0cf7c3ae246
amy-e-stone <aestone04@gmail.com>
2025-08-14 15:28:28 -0400

    Removed transform effects from Register button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,10 @@ export default function Header() {
     <header className="flex justify-between items-center p-4 bg-gray-100">
       <Logo />
       <div className="space-x-3">
-        <button className="w-24 py-1 text-[#7209b7] hover:text-[#4361ee]">
+        <button className="w-24 py-1 text-[#7209b7] hover:text-[#4361ee] cursor-pointer">
           Login
         </button>
-        <button className="w-24 py-1 bg-[#7209b7] text-gray-100 rounded-full transform-gpu will-change-transform transition delay-150 duration-300 ease-in-out hover:scale-110 hover:bg-[#4361ee]">
+        <button className="w-24 py-1 bg-[#7209b7] text-gray-100 font-semibold rounded-full duration-300 hover:bg-[#4361ee] cursor-pointer">
           Register
         </button>
       </div>
