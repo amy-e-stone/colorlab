52ba26c2d9b3576a663ac6192e8d1fe6c4cb5ea0
amy-e-stone <aestone04@gmail.com>
2025-08-14 15:27:04 -0400

    Styled color picker

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -9,25 +9,33 @@ function Demo() {
   const [hsva, setHsva] = useState({ h: 214, s: 43, v: 90, a: 1 });
   return (
     <Fragment>
-      <Wheel
-        color={hsva}
-        onChange={(color) => setHsva({ ...hsva, ...color.hsva })}
-      />
-      <ShadeSlider
-        hsva={hsva}
-        style={{ width: 210, marginTop: 20 }}
-        onChange={(newShade) => {
-          setHsva({ ...hsva, ...newShade });
-        }}
-      />
-      <div
-        style={{
-          width: "100%",
-          height: 34,
-          marginTop: 20,
-          background: hsvaToHex(hsva),
-        }}
-      ></div>
+      <div className="w-fit mx-auto text-center">
+        <h1 className="text-2xl mb-10 mt-10">Pick a color to get started!</h1>
+        <Wheel
+          className="cursor-pointer"
+          width={360}
+          height={360}
+          color={hsva}
+          onChange={(color) => setHsva({ ...hsva, ...color.hsva })}
+        />
+        <ShadeSlider
+          className="cursor-pointer"
+          hsva={hsva}
+          style={{ width: 360, marginTop: 20 }}
+          onChange={(newShade) => {
+            setHsva({ ...hsva, ...newShade });
+          }}
+        />
+        <div
+          className="mb-10"
+          style={{
+            width: "360px",
+            height: 200,
+            marginTop: 20,
+            background: hsvaToHex(hsva),
+          }}
+        ></div>
+      </div>
     </Fragment>
   );
 }
