a7e9623acf0e536891ce5d1968497a737b8c1bc4
amy-e-stone <aestone04@gmail.com>
2025-09-25 11:59:49 -0400

    Corrected stretching of card caused by adding new elements

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -20,80 +20,86 @@ function ColorPicker() {
         <h1 className="text-2xl mb-10 mt-10">Pick a color to get started!</h1>
 
         {/* HEX user input */}
-        <input
-          name="hex"
-          type="text"
-          className="border px-2 py-1 mb-4"
-          placeholder="#000000"
-          onChange={(event) => {
-            const val = event.target.value;
-            if (/^#?[0-9A-Fa-f]{6}$/.test(val)) {
-              const hex = val.startsWith("#") ? val : `#${val}`;
-              const newHsva = hexToHsva(hex);
-              setHsva(newHsva);
-            }
-          }}
-        />
+        <div className="w-[360px] mx-auto">
+          <input
+            name="hex"
+            type="text"
+            className="border px-2 py-1 mb-4"
+            placeholder="#000000"
+            onChange={(event) => {
+              const val = event.target.value;
+              if (/^#?[0-9A-Fa-f]{6}$/.test(val)) {
+                const hex = val.startsWith("#") ? val : `#${val}`;
+                const newHsva = hexToHsva(hex);
+                setHsva(newHsva);
+              }
+            }}
+          />
+        </div>
 
         {/* RGB user input */}
-        <input
-          name="rgb"
-          type="text"
-          className="border px-2 py-1 mb-4 ml-4"
-          placeholder="R, G, B"
-          onChange={(event) => {
-            const val = event.target.value;
-            const match = val.match(/^(\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})$/);
-            // If the returned array is not null ...
-            if (match) {
-              // match returns an array so destructuring is position-based - with the first element being the full match, followed by the components of the match
-              const [fullMatch, rStr, gStr, bStr] = match;
-              const r = Number(rStr);
-              const g = Number(gStr);
-              const b = Number(bStr);
-              // Ensure all values are within valid RGB range
-              // Callback function where the function passed as the param is run for every element in the array literal
-              if ([r, g, b].every((n) => n >= 0 && n <= 255)) {
-                const newHsva = rgbaToHsva({ r, g, b });
-                setHsva(newHsva);
+        <div className="w-[360px] mx-auto">
+          <input
+            name="rgb"
+            type="text"
+            className="border px-2 py-1 mb-4"
+            placeholder="R, G, B"
+            onChange={(event) => {
+              const val = event.target.value;
+              const match = val.match(/^(\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3})$/);
+              // If the returned array is not null ...
+              if (match) {
+                // match returns an array so destructuring is position-based - with the first element being the full match, followed by the components of the match
+                const [fullMatch, rStr, gStr, bStr] = match;
+                const r = Number(rStr);
+                const g = Number(gStr);
+                const b = Number(bStr);
+                // Ensure all values are within valid RGB range
+                // Callback function where the function passed as the param is run for every element in the array literal
+                if ([r, g, b].every((n) => n >= 0 && n <= 255)) {
+                  const newHsva = rgbaToHsva({ r, g, b });
+                  setHsva(newHsva);
+                }
               }
-            }
-          }}
-        />
+            }}
+          />
+        </div>
 
         {/* HSL user input */}
-        <input
-          name="hsl"
-          type="text"
-          className="border px-2 py-1 mb-4 ml-4"
-          placeholder="H, S%, L%"
-          onChange={(event) => {
-            const val = event.target.value;
-            const match = val.match(
-              /^(\d{1,3}),\s*(\d{1,3})%?,\s*(\d{1,3})%?$/
-            );
-            // If the returned array is not null ...
-            if (match) {
-              // match returns an array so destructuring is position-based - with the first element being the full match, followed by the components of the match
-              const [fullMatch, hStr, sStr, lStr] = match;
-              const h = Number(hStr);
-              const s = Number(sStr);
-              const l = Number(lStr);
-              // Ensure all values are within valid HSL ranges (not all have the same range, so we do not apply a callback function here)
-              if (
-                h >= 0 &&
-                h <= 360 &&
-                s >= 0 &&
-                s <= 100 &&
-                l >= 0 &&
-                l <= 100
-              ) {
-                const newHsva = hslaToHsva({ h, s, l });
-                setHsva(newHsva);
+        <div className="w-[360px] mx-auto">
+          <input
+            name="hsl"
+            type="text"
+            className="border px-2 py-1 mb-4"
+            placeholder="H, S%, L%"
+            onChange={(event) => {
+              const val = event.target.value;
+              const match = val.match(
+                /^(\d{1,3}),\s*(\d{1,3})%?,\s*(\d{1,3})%?$/
+              );
+              // If the returned array is not null ...
+              if (match) {
+                // match returns an array so destructuring is position-based - with the first element being the full match, followed by the components of the match
+                const [fullMatch, hStr, sStr, lStr] = match;
+                const h = Number(hStr);
+                const s = Number(sStr);
+                const l = Number(lStr);
+                // Ensure all values are within valid HSL ranges (not all have the same range, so we do not apply a callback function here)
+                if (
+                  h >= 0 &&
+                  h <= 360 &&
+                  s >= 0 &&
+                  s <= 100 &&
+                  l >= 0 &&
+                  l <= 100
+                ) {
+                  const newHsva = hslaToHsva({ h, s, l });
+                  setHsva(newHsva);
+                }
               }
-            }
-          }}
-        />
+            }}
+          />
+        </div>
 
         <Wheel
           className="cursor-pointer"
