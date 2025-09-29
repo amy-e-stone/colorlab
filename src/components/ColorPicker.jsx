b6cd555731830f5318017ec00026a8cc042d87a4
amy-e-stone <aestone04@gmail.com>
2025-09-29 08:43:57 -0400

    Corrected the use of fragment component

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,6 +1,5 @@
 /* Base code gotten from https://uiwjs.github.io/react-color/#/wheel */
 
-import { useState, Fragment } from "react";
 import Wheel from "@uiw/react-color-wheel";
 import ShadeSlider from "@uiw/react-color-shade-slider";
 import HexInput from "./HexInput";
@@ -12,33 +11,31 @@ import Card from "./Card";
 
 function ColorPicker({ hsva, setHsva }) {
   return (
-    <Fragment>
-      <Card>
-        <div className="w-fit mx-auto text-center">
-          <h1 className="text-2xl mb-10 mt-10">Pick a color to get started!</h1>
-          <HexInput setHsva={setHsva} />
-          <RgbInput setHsva={setHsva} />
-          <HslInput setHsva={setHsva} />
-          <Wheel
-            className="cursor-pointer"
-            width={360}
-            height={360}
-            color={hsva}
-            onChange={(color) => setHsva({ ...hsva, ...color.hsva })}
-          />
-          <ShadeSlider
-            className="cursor-pointer"
-            hsva={hsva}
-            style={{ width: 360, marginTop: 20 }}
-            onChange={(newShade) => {
-              setHsva({ ...hsva, ...newShade });
-            }}
-          />
-          <ColorDisplay hsva={hsva} />
-          <ColorValueDisplay hsva={hsva} />
-        </div>
-      </Card>
-    </Fragment>
+    <Card>
+      <div className="w-fit mx-auto text-center">
+        <h1 className="text-2xl mb-10 mt-10">Pick a color to get started!</h1>
+        <HexInput setHsva={setHsva} />
+        <RgbInput setHsva={setHsva} />
+        <HslInput setHsva={setHsva} />
+        <Wheel
+          className="cursor-pointer"
+          width={360}
+          height={360}
+          color={hsva}
+          onChange={(color) => setHsva({ ...hsva, ...color.hsva })}
+        />
+        <ShadeSlider
+          className="cursor-pointer"
+          hsva={hsva}
+          style={{ width: 360, marginTop: 20 }}
+          onChange={(newShade) => {
+            setHsva({ ...hsva, ...newShade });
+          }}
+        />
+        <ColorDisplay hsva={hsva} />
+        <ColorValueDisplay hsva={hsva} />
+      </div>
+    </Card>
   );
 }
 
