4725513e887c0682be8c5da6ec76760ff2543493
amy-e-stone <aestone04@gmail.com>
2025-09-29 12:56:36 -0400

    Adjusted color palette to be dynamic in the amount of colors it receives

--- src/components/ColorPalette.jsx
+++ src/components/ColorPalette.jsx
@@ -1,15 +1,24 @@
-export default function ColorPalette({ colors = [] }) {
+import { Fragment } from "react";
+
+export default function ColorPalette({ colors }) {
   return (
     <div
       className="rounded-md overflow-hidden"
       style={{ height: 120, width: 400 }}
     >
-      <div className="grid grid-cols-5 h-full">
-        <div style={{ backgroundColor: colors[0] || "#aaaaaa" }} />
-        <div style={{ backgroundColor: colors[1] || "#bbbbbb" }} />
-        <div style={{ backgroundColor: colors[2] || "#cccccc" }} />
-        <div style={{ backgroundColor: colors[3] || "#dddddd" }} />
-        <div style={{ backgroundColor: colors[4] || "#eeeeee" }} />
+      <div
+        className="grid h-full"
+        style={{ gridTemplateColumns: `repeat(${colors.length}, 1fr)` }} // repeat(count, size)
+      >
+        {colors.length > 0 && (
+          <Fragment>
+            {colors[0] && <div style={{ backgroundColor: colors[0] }} />}
+            {colors[1] && <div style={{ backgroundColor: colors[1] }} />}
+            {colors[2] && <div style={{ backgroundColor: colors[2] }} />}
+            {colors[3] && <div style={{ backgroundColor: colors[3] }} />}
+            {colors[4] && <div style={{ backgroundColor: colors[4] }} />}
+          </Fragment>
+        )}
       </div>
     </div>
   );
