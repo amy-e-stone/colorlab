665e19932adaf583433d8d4cc268a6bead50d375
amy-e-stone <aestone04@gmail.com>
2025-10-08 14:32:25 -0400

    Implemented Lombok in User and Palette entities

--- colorlab-backend/src/main/java/com/colorlab/backend/model/User.java
+++ colorlab-backend/src/main/java/com/colorlab/backend/model/User.java
@@ -1,11 +1,14 @@
 package com.colorlab.backend.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,18 +18,4 @@ public class User {
 
     @Column(nullable = false)
     private String password;
-
-    // Getters and setters
-    public Long getId() { return id; }
-
-    public void setId(Long id) { this.id = id; }
-
-    public String getUsername() { return username; }
-
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-
-    public void setPassword(String password) { this.password = password; }
 }
-
