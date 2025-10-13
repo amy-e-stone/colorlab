df7b7e36d987c9756d81a2fd22d878d83e07bf4c
amy-e-stone <aestone04@gmail.com>
2025-10-13 14:51:00 -0400

    Changed links to match new routing

--- colorlab-frontend/src/components/Header.jsx
+++ colorlab-frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="p-4 bg-gray-100">
       <div className="flex w-full max-w-6xl mx-auto justify-between items-center">
         {/* Unified Logo Wrapper */}
-        <Link to="/ColorLab" className="block">
+        <Link to="/" className="block">
           <div className="sm:block hidden">
             <Logo height={40} />
           </div>
@@ -19,13 +19,13 @@ export default function Header() {
         {/* Login and Register buttons */}
         <div className="flex space-x-2">
           <Link
-            to="ColorLab/login"
+            to="login"
             className="inline-flex w-20 py-1 items-center justify-center text-sm text-[#7209b7] hover:text-[#4361ee] cursor-pointer sm:w-24 sm:text-base"
           >
             Login
           </Link>
           <div className="text-sm w-20 sm:text-base sm:w-24">
-            <Button buttontext="Register" to="ColorLab/register" />
+            <Button buttontext="Register" to="register" />
           </div>
         </div>
       </div>
